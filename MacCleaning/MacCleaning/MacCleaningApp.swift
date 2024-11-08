2d0e1fb8911a269a7e134d66c63a533a1fc75d8f
Gloridust <Gloridust@gmail.com>
2024-11-09 00:23:31 +0800

    init

---
+++ MacCleaning/MacCleaning/MacCleaningApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacCleaningApp.swift
+//  MacCleaning
+//
+//  Created by Ethan Zou on 2024/11/9.
+//
+
+import SwiftUI
+
+@main
+struct MacCleaningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
